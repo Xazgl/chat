3461ead11a54abf25db995e545f946e8b8e2abd0
Uriy Jykov <ldomofon-sericel@rambler.ru>
2021-08-05 22:01:06 +0300

    05.08.2021ty

--- server/app.js
+++ server/app.js
@@ -45,9 +45,19 @@ webSocketServer.on('connection',function(ws) {
 
 
 // request - получаем данные из формы .response - отсылает данные пользователю
-app.get('/test-form',function(request,response) {
-   response.send(request.query)
-}) 
+//app.get('/test-form',function(request,response) {
+  // response.send(request.query)
+//}) 
+
+app.use(express.json())
+
+app.post('/chat',function (request, response) {
+   console.log(request.body);
+   response.send(request.body)
+
+
+})
+
 
 app.use(express.static('../client'))
 
